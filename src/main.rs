9a475aff7b712adf7aeb22c0dce2256e9dfa03db
Kirill Kobylyanskiy <k.kobylyanskiy@gmail.com>
2026-02-08 18:59:55 +0100

    remove commented code

--- src/main.rs
+++ src/main.rs
@@ -26,14 +26,6 @@ async fn main() {
         .with(tracing_subscriber::EnvFilter::new(filter_str))
         .with(tracing_subscriber::fmt::layer())
         .init();
-    // tracing_subscriber::registry()
-    //     .with(
-    //         tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
-    //             format!("{}=debug,tower_http=debug", env!("CARGO_CRATE_NAME")).into()
-    //         }),
-    //     )
-    //     .with(tracing_subscriber::fmt::layer())
-    //     .init();
 
     let client = reqwest::Client::new();
     let listen_address = cfg.server.listen_address.clone();
